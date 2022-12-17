1cd0d5dce07a7e431b46ceab85f93940b0a50abc
Alex100688 <alessiozingaro2@gmail.com>
2022-12-17 15:20:31 +0100

    Trasforma il file api.php in un array associativo

--- api.php
+++ api.php
@@ -1,5 +1,6 @@
 <?php
  $albums= file_get_contents('dischi.json');
+ $albums = json_decode($albums, true);
  var_dump($albums);die;
  header("Content-Type: application/json");
  echo json_encode($albums)
