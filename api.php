892f2faec2348bfe2a164a6154427a5690c275c6
Alex100688 <alessiozingaro2@gmail.com>
2022-12-17 15:16:35 +0100

    Aggiunge un database.json

--- api.php
+++ api.php
@@ -1,17 +1,6 @@
 <?php
-
+ $albums= file_get_contents('dischi.json');
+ var_dump($albums);die;
+ header("Content-Type: application/json");
+ echo json_encode($albums)
 ?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Api</title>
-</head>
-<body>
-    <h1>ciao a tutti</h1>
-</body>
-</html>
